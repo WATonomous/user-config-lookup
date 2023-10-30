5424f044deff6d20ed1a574a51b0936c1229141c
Jimmy Jiang <j257jian@uwaterloo.ca>
2023-10-30 02:24:26 +0000

    only parse files in "data" directory

--- app/utils.py
+++ app/utils.py
@@ -20,9 +20,13 @@ def generate_email_map() -> Union[dict, None]:
     logger.info(f"Path to user configs: {path}")
     email_map = {}
     for file in os.scandir(path):
+        if os.path.isdir(file):
+            logger.warn(f"Skipping folder at path: {path}")
+            continue
         user_config = yaml.safe_load(open(file))
         contact_emails: list[str] = user_config["general"]["contact_emails"]
         for contact_email in contact_emails:
             contact_email = contact_email.lower()
             email_map[contact_email] = os.path.abspath(file)
+    logger.info(f"email map size: {len(email_map)}")
     return email_map
